a74eb8763e0a9051e311a0c4435783e39f4420ef
junssg <80884697+junssg@users.noreply.github.com>
2022-06-09 01:28:38 +0900

    Update junseok123.cpp

--- OpenGLProject/junseok123.cpp
+++ OpenGLProject/junseok123.cpp
@@ -1,374 +1,365 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <Windows.h>
-#include <stdio.h>
-#include <GL/glut.h>
-#include <GL/glu.h>
+#include <gl/glut.h>
+#include <math.h>
+#include <gl/gl.h>
+#include <gl/glu.h>
 #include "glaux.h"
-//for drawing fonts
+#include <stdlib.h>
 #include "bitmap_fonts.h"
 
 #include <vector>
 #include <glm/glm.hpp> 
 #include <glm/gtc/matrix_transform.hpp> 
 #include <glm/gtc/type_ptr.hpp>
+using namespace std;
+using namespace glm;
+static float ang = 0.0, ratio;
+static float x = 0.0f, y = 1.75f, z = 5.0f;
+static float lx = 0.0f, ly = 0.0f, lz = -1.0f;
 
-static POINT	ptLastMousePosit;
-static POINT	ptCurrentMousePosit;
-static bool		bMousing;
-
+static char coor[255];
 static char message[255];
-
+GLfloat dx = 0.2;
+GLfloat x1;
 int    g_nWindowWidth;
 int    g_nWindowHeight;
 
-float g_fDistance = -5.0f;
-float g_fSpinX = 0.0f;
-float g_fSpinY = 0.0f;
+unsigned int MyTextureObject[1];
+AUX_RGBImageRec* pTextureImage[1];
+GLuint g_textureID = -1;
 
+void DrawCube(float size);
+void addMob(vec3 position, float size);
+void AddContactForce();
+void DrawCubeTex();
 
-glm::vec4 ConvertVector(glm::vec4 p)
-{
-	glm::mat4 model(1.f);
-	model = glm::translate(model, glm::vec3(0.0f, 0.0f, g_fDistance));
-	model = glm::rotate(model, glm::radians(-g_fSpinY), glm::vec3(1.0f, 0.0f, 0.0f));
-	model = glm::rotate(model, glm::radians(-g_fSpinX), glm::vec3(0.0f, 1.0f, 0.0f));
+bool die = false;
 
-	glm::mat4 view = glm::mat4(1.f);
+typedef struct {
+	float Left, Right;
+	float Top, Bottom;
+}Rect;
+typedef struct {
+	vec3 p = vec3(x, y, z);
+}player;
+player Player;
 
-	glm::mat4 projection;
-	projection = glm::perspective(glm::radians(45.f),
-		(float)g_nWindowWidth / (float)g_nWindowHeight, 0.1f, 100.0f);
-	glm::mat4 M = projection * view * model;
-	glm::mat4 invM = inverse(M);
-	glm::vec4 convP;
+struct Mob{
+	vec3 p; //position
+	vec3 v; //velocity
+	vec3 force; //force
+	float size; //size
+	float m; //mass
+};
 
-	convP = invM * p;
-	convP /= convP[3];
+vector<Mob> mobs;
 
-
-	return convP;
-}
-float u = 50;
-float v = 50;
-void DrawUI()
-{
-	glDisable(GL_LIGHTING);
-
-	//Draw box UI
-	glPushName(106);
-	glColor4f(0, 0, 0.5, 0.5);
-	glBegin(GL_QUADS);
-	glm::vec4 convP = ConvertVector(glm::vec4(-1, -1, 0, 1));
-	glVertex4f(convP[0], convP[1], convP[2], 1.f);
-
-	convP = ConvertVector(glm::vec4(1, -1, 0, 1));
-	glVertex4f(convP[0], convP[1], convP[2], 1.f);
-
-	convP = ConvertVector(glm::vec4(1, -0.5, 0, 1));
-	glVertex4f(convP[0], convP[1], convP[2], 1.f);
-
-	convP = ConvertVector(glm::vec4(-1, -0.5, 0, 1));
-	glVertex4f(convP[0], convP[1], convP[2], 1.f);
-
-	glEnd();
-	glPopName();
-
-	//Draw font
-	beginRenderText(g_nWindowWidth, g_nWindowHeight);
-	{
-		glColor3f(1.0f, 1.0f, 1.0f);
-		u = (1.f - 0.75f) / 2.f * g_nWindowWidth;
-		v = (1.f - (-0.75f)) / 2.f * g_nWindowHeight;
-		renderText(u, v, BITMAP_FONT_TYPE_HELVETICA_18, message);
+AUX_RGBImageRec* LoadBMP(const char* szFilename) {
+	FILE* pFile = NULL;
+	if (!szFilename) {
+		return NULL;
 	}
-	endRenderText();
 
-	glEnable(GL_LIGHTING);
+	pFile = fopen(szFilename, "r");
+	if (pFile) {
+		fclose(pFile);
+		return auxDIBImageLoad(szFilename);     //파일로부터 메모리로
+	}
+	return NULL;
 }
+int LoadGLTextures(const char* szFilePath) {       //파일을 로드하고 텍스쳐로 변환
+	int Status = FALSE;
+	glClearColor(0.0, 0.0, 0.0, 0.5);
+	memset(pTextureImage, 0, sizeof(void*) * 1);    //포인터를 널로
 
-void DrawGLScene() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glMatrixMode(GL_MODELVIEW);
+	if (pTextureImage[0] = LoadBMP(szFilePath)) {   //비트맵을 로드하고 오류확인
+		Status = TRUE;                              //상태 플랙을 True로
+		glGenTextures(1, &MyTextureObject[0]);      //텍스쳐 생성
+		glBindTexture(GL_TEXTURE_2D, MyTextureObject[0]);
+		glTexImage2D(GL_TEXTURE_2D, 0, 3,
+			pTextureImage[0]->sizeX, pTextureImage[0]->sizeY,
+			0, GL_RGB, GL_UNSIGNED_BYTE, pTextureImage[0]->data);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glEnable(GL_TEXTURE_2D);
+	}
+	if (pTextureImage[0]) {                 //텍스쳐가 존재하면
+		if (pTextureImage[0]->data) {       //텍스쳐 영상이 존재하면
+			free(pTextureImage[0]->data);   //텍스쳐 영상공간 반납
+		}
+		free(pTextureImage[0]);             //텍스쳐 반납
+	}
+	return Status;
+}
+void changeSize(int w, int h)
+{
+	if (h == 0)
+		h = 1;
+
+	ratio = 1.0f * w / h;
+	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 
-	glTranslatef(0.0f, 0.0f, g_fDistance);
-	glRotatef(-g_fSpinY, 1.0f, 0.0f, 0.0f);
-	glRotatef(-g_fSpinX, 0.0f, 1.0f, 0.0f);
 
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glViewport(0, 0, w, h);
 
-	float mat[16];
-	glm::f32 values[16];
+	gluPerspective(45, ratio, 1, 1000);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0.0f, 1.0f, 0.0f);
 
 
-	glInitNames();
-	glEnable(GL_LIGHTING);
+}
 
-	glPushName(100);
-	glPushMatrix();
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glutSolidCube(0.3);
-	glPopMatrix();
-	glPopName();
 
-	glPushName(101);
-	glPushMatrix();
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glTranslatef(-1.0f, -1.0f, 0.0f);
-	glutSolidCube(0.3);
-	glPopMatrix();
-	glPopName();
+void drawCircle() {
 
-	glPushName(102);
-	glPushMatrix();
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glTranslatef(1.0f, 1.0f, 0.0f);
-	glutSolidCube(0.3);
-	glPopMatrix();
-	glPopName();
+	
+	glColor3f(1.0f, 0.7f, 1.0f);
+	glTranslatef(0.0f, 5.0f, 0.0f);
+	glutSolidSphere(0.75f, 20, 20);
 
-	glPushName(103);
-	glPushMatrix();
-	glColor3f(1.0f, 1.0f, 0.0f);
-	glTranslatef(-1.0f, 1.0f, 0.0f);
-	glutSolidCube(0.3);
-	glPopMatrix();
-	glPopName();
 
-	glPushName(104);
-	glPushMatrix();
-	glColor3f(0.0f, 1.0f, 1.0f);
-	glTranslatef(1.0f, -1.0f, 0.0f);
-	glutSolidCube(0.3);
-	glPopMatrix();
-	glPopName();
+}
 
-	glDisable(GL_LIGHTING);
+void MyTimer(int value) {
+	Player.p = vec3(x, y, z);
+	glutPostRedisplay();
+	AddContactForce();
+	if (die == false)
+		glutTimerFunc(40, MyTimer, 1);
+	else
+	{
+		x = 300;
+		
+		z = 300;
+	}
+	
+}
+void DrawCube(float size)
+{
+	glColor3f(0.0f, 0.9f, 0.9f);
+	glBegin(GL_QUADS);
+	glVertex3f(-size, -size, size);		// 앞면
+	glVertex3f(size, -size, size);
+	glVertex3f(size, size, size);
+	glVertex3f(-size, size, size);
 
-	glPushName(105);
-	glColor3f(0.7f, 0.7f, 1.0f);
-	glBegin(GL_LINES);
-	glVertex2f(-1.0f, -1.0f);
-	glVertex2f(1.0f, -1.0f);
+	glVertex3f(size, -size, -size);
+	glVertex3f(-size, -size, -size);		// 뒷면
+	glVertex3f(-size, size, -size);
+	glVertex3f(size, size, -size);
 
-	glVertex2f(1.0f, -1.0f);
-	glVertex2f(1.0f, 1.0f);
+	glVertex3f(-size, size, size);		// 윗면
+	glVertex3f(size, size, size);
+	glVertex3f(size, size, -size);
+	glVertex3f(-size, size, -size);
 
-	glVertex2f(1.0f, 1.0f);
-	glVertex2f(-1.0f, 1.0f);
+	glVertex3f(-size, -size, -size);		// 아랫면
+	glVertex3f(size, -size, -size);
+	glVertex3f(size, -size, size);
+	glVertex3f(-size, -size, size);
 
-	glVertex2f(-1.0f, 1.0f);
-	glVertex2f(-1.0f, -1.0f);
+	glVertex3f(size, -size, size);		// 우측면
+	glVertex3f(size, -size, -size);
+	glVertex3f(size, size, -size);
+	glVertex3f(size, size, size);
 
-	glVertex2f(0.0f, 1.5f);
-	glVertex2f(0.0f, -1.5f);
+	glVertex3f(-size, -size, -size);	// 좌측면
+	glVertex3f(-size, -size, size);
+	glVertex3f(-size, size, size);
+	glVertex3f(-size, size, -size);
+}
 
-	glVertex2f(1.5f, 0.0f);
-	glVertex2f(-1.5f, 0.0f);
+void renderScene(void) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glVertex2f(1.5f, 1.5f);
-	glVertex2f(-1.5f, -1.5f);
-	glEnd();
-	glPopName();
 
-	glColor3f(1.0f, 1.0f, 1.0f);
-	glBegin(GL_LINES);
-	glVertex2f(-1.5f, 1.5f);
-	glVertex2f(1.5f, -1.5f);
+
+	glColor3f(0.9f, 0.9f, 0.9f);
+	glBegin(GL_QUADS);
+	glVertex3f(-100.0f, 0.0f, -100.0f);
+	glVertex3f(-100.0f, 0.0f, 100.0f);
+	glVertex3f(100.0f, 0.0f, 100.0f);
+	glVertex3f(100.0f, 0.0f, -100.0f);
 	glEnd();
 
+	//DrawCube(1.0f);
+	//addMob(vec3(x,y,z-1),0.2);
+	DrawCubeTex();
+	
+	glEnd();
+	
+	sprintf(coor,"x : %f y : %f z : %f",x,y,z);
+	sprintf(message, "You DIED");
 	beginRenderText(g_nWindowWidth, g_nWindowHeight);
 	{
-		glColor3f(1.0f, 1.0f, 1.0f);
-		u = (1.f - 0.75f) / 2.f * g_nWindowWidth;
-		v = (1.f - (-0.75f)) / 2.f * g_nWindowHeight;
-		renderText(u, v, BITMAP_FONT_TYPE_HELVETICA_18, "hello");
+		glColor3f(1.f, 1.f, 1.0f);
+		int u = (1.f - 0.75f) / 4.f * g_nWindowWidth;
+		int v = (1.f - (-1.75f)) / 4.f * g_nWindowHeight;
+		if (die==false)
+			renderText(g_nWindowWidth - 0.95, g_nWindowHeight + 0.9, BITMAP_FONT_TYPE_HELVETICA_18, coor);
+		else
+			renderText(g_nWindowWidth-0.08 , g_nWindowHeight, BITMAP_FONT_TYPE_HELVETICA_18,message);
 	}
 	endRenderText();
-	//Draw UI
-	DrawUI();
-
 	glutSwapBuffers();
 }
 
-void ProcessSelect(GLuint index[64]) {
-	switch (index[3]) {
-	case 100: MessageBox(NULL, "Red Wire Teapot", "Selection", MB_OK); break;
-	case 101: MessageBox(NULL, "Green Solid Sphere", "Selection", MB_OK); break;
-	case 102: MessageBox(NULL, "Blue Solid Sphere", "Selection", MB_OK); break;
-	case 103: MessageBox(NULL, "Yellow Solid Sphere", "Selection", MB_OK); break;
-	case 104: MessageBox(NULL, "Cyan Solid Sphere", "Selection", MB_OK); break;
-	case 105: MessageBox(NULL, "Line", "Selection", MB_OK); break;
+void orientMe(float ang) {
 
-	default: MessageBox(NULL, "None?", "Selection", MB_OK); break;
-	}
-}
 
-void SetTextMessage(GLuint index[64])
-{
-	switch (index[3]) {
-
-	case 100: sprintf_s(message, "center"); break;
-	case 101: sprintf_s(message, "Green Solid Sphere"); break;
-	case 102: sprintf_s(message, "Blue Solid Sphere"); break;
-	case 103: sprintf_s(message, "Yellow Solid Sphere"); break;
-	case 104: sprintf_s(message, "Cyan Solid Sphere"); break;
-	case 105: sprintf_s(message, "Line"); break;
-	case 106: sprintf_s(message, "UI"); break;
-
-		//default: sprintf_s(message, "None"); break;
-	}
-}
-
-void SelectObjects(GLint x, GLint y) {
-	GLuint selectBuff[64];
-	GLint hits, viewport[4];
-
-	glSelectBuffer(64, selectBuff);
-	glGetIntegerv(GL_VIEWPORT, viewport);
-	glMatrixMode(GL_PROJECTION);
-	glPushMatrix();
-	glRenderMode(GL_SELECT);
+	lx = sin(ang);
+	lz = -cos(ang);
 	glLoadIdentity();
-	gluPickMatrix(x, viewport[3] - y, 2, 2, viewport);
+	gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0.0f, 1.0f, 0.0f);
+	sprintf(coor, "x : %f y : %f z : %f", x, y, z);
+}
 
-	gluPerspective(45.0f, (GLfloat)g_nWindowWidth / (GLfloat)g_nWindowHeight, 0.1f, 100.0f);
-	glMatrixMode(GL_MODELVIEW);
+
+void moveMeFlat(int i) {
+	x = x + i * (lx) * 0.1;
+	z = z + i * (lz) * 0.1;
 	glLoadIdentity();
-
-	DrawGLScene();
-
-	hits = glRenderMode(GL_RENDER);
-	if (hits > 0)
-	{
-		//ProcessSelect(selectBuff);
-		SetTextMessage(selectBuff);
-	}
-
-	glMatrixMode(GL_PROJECTION);
-	glPopMatrix();
-	glMatrixMode(GL_MODELVIEW);
+	gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0.0f, 1.0f, 0.0f);
+	sprintf(coor, "x : %f y : %f z : %f", x, y, z);
 }
 
-void MyMouse(int button, int state, int x, int y) {
-	switch (button) {
-	case GLUT_LEFT_BUTTON:
-		if (state == GLUT_DOWN) {
-			ptLastMousePosit.x = ptCurrentMousePosit.x = x;
-			ptLastMousePosit.y = ptCurrentMousePosit.y = y;
-			SelectObjects(ptCurrentMousePosit.x, ptCurrentMousePosit.y);
-			bMousing = true;
-		}
-		else
-			bMousing = false;
-		break;
-	case GLUT_MIDDLE_BUTTON:
-	case GLUT_RIGHT_BUTTON:
-		break;
-	default:
-		break;
-	}
+void processNormalKeys(unsigned char key, int x, int y) {
 
-	glutPostRedisplay();
-}
-
-void MyMotion(int x, int y) {
-	ptCurrentMousePosit.x = x;
-	ptCurrentMousePosit.y = y;
-
-	if (bMousing)
-	{
-		g_fSpinX -= (ptCurrentMousePosit.x - ptLastMousePosit.x);
-		g_fSpinY -= (ptCurrentMousePosit.y - ptLastMousePosit.y);
-	}
-
-	ptLastMousePosit.x = ptCurrentMousePosit.x;
-	ptLastMousePosit.y = ptCurrentMousePosit.y;
-
-	glutPostRedisplay();
-}
-
-int InitGL() {
-	glShadeModel(GL_SMOOTH);
-	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
-	glClearDepth(1.0f);
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_COLOR_MATERIAL);
-	glEnable(GL_LIGHT0);
-	glDepthFunc(GL_LEQUAL);
-	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
-
-	return TRUE;
-}
-
-void MyReshape(int w, int h) {
-	g_nWindowWidth = w;
-	g_nWindowHeight = h;
-
-	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(45, (GLfloat)w / (GLfloat)h, 0.1, 100.0);
-}
-void MyMainMenu(int entryID) {
-	if (entryID == 1)
+	if (key == 27)
 		exit(0);
-	glutPostRedisplay(); // 프로그램 종료
 }
-void MySpecial(int key, int x, int y) {
-	if (key == GLUT_KEY_UP) {
-		g_fDistance -= 0.1f;
+
+
+void inputKey(unsigned char key, int x, int y) {
+
+	switch (key) {
+
+	case 'a':
+		ang -= 0.03f;
+		orientMe(ang);
+		break;
+	case 'd':
+		ang += 0.03f;
+		orientMe(ang);
+		break;
+	case 'w':
+		moveMeFlat(1);
+		break;
+	case 's':
+		moveMeFlat(-1);
+		break;
 	}
-	else if (key == GLUT_KEY_DOWN) {
-		g_fDistance += 0.1f;
+}
+void addMob(vec3 position, float size)
+{
+	Mob newmob;
+
+	vec3 _pos(0, 0, 0);
+	_pos.x = position.x;
+	_pos.y = position.y;
+	_pos.z = position.z;
+	float _size = size;
+	newmob.p.x = _pos.x;
+	newmob.p.y = _pos.y;
+	newmob.p.z = _pos.z;
+	newmob.size = _size;
+	
+	
+	DrawCube(size);
+	
+
+	mobs.push_back(newmob);
+}
+
+void AddContactForce()
+{
+	for (int i = 0; i < mobs.size(); i++)
+	{
+		for (int j = i + 1; j < mobs.size(); j++)
+		{
+			vec3 dis = mobs[i].p - Player.p;       //dis=반지름 사이
+			//compute length
+			float L = length(dis);                    //L= 반지름 사이의 거리
+
+			dis = normalize(dis);
+			if (L <= mobs[i].size)          //L이 반지름 사이의 거리보다 작다? => 두 공이 겹쳤다
+			{
+				die = true;
+				//compute and add force
+				/*vec3 force = stiff * ((balls[i].r + balls[j].r) - L) * dis;
+				balls[i].force += force;
+				balls[j].force -= force;*/
+			}
+		}
 	}
-
-	glutPostRedisplay();
 }
-void MySubMenu_shape(int entryID) {
-	if (entryID == 1)
-		sprintf_s(message, "hoho");
-	else if (entryID == 2)
-		sprintf_s(message, "haha");
-	glutPostRedisplay();
+void DrawCubeTex() {
+	glBindTexture(GL_TEXTURE_2D, MyTextureObject[0]);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 1.0f);  //앞면
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, 1.0f, 1.0f);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
+
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);  //뒷면
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f, 1.0f, -1.0f);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, -1.0f);
+
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);  //윗면
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, 1.0f, 1.0f);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, 1.0f, -1.0f);
+
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);  //아랫면
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f, -1.0f, -1.0f);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
+
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, -1.0f);  //우측면
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, 1.0f, -1.0f);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f, 1.0f, 1.0f);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);
+
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);  //좌측면
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);
+	glEnd();
+
+
+	glBindTexture(GL_TEXTURE_2D, g_textureID);
+
 }
 
-void MySubMenu_size(int entryID) {
-	if (entryID == 1)
-		sprintf_s(message, "heoheo");
-	else if (entryID == 2)
-		sprintf_s(message, "hihi");
-	glutPostRedisplay();
-}
-
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(640, 480);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("test");
-	GLint MySubMenuID_shape = glutCreateMenu(MySubMenu_shape);
-	glutAddMenuEntry("hoho", 1);
-	glutAddMenuEntry("haha", 2);
+	glutInitWindowSize(900, 700);
+	glutCreateWindow("project");
+	
 
-	//Change Size 서브 메뉴의 콜백 함수 등록
-	GLint MySubMenuID_size = glutCreateMenu(MySubMenu_size);
-	glutAddMenuEntry("heoheo", 1);
-	glutAddMenuEntry("hihi", 2);
+	glutKeyboardFunc(processNormalKeys);
+	glutKeyboardFunc(inputKey);
 
-	GLint MyMainMenuID = glutCreateMenu(MyMainMenu);
-	glutAddSubMenu("Change Shape", MySubMenuID_shape);
-	glutAddSubMenu("Change Size", MySubMenuID_size);
-	glutAddMenuEntry("Exit", 1);
+	glutDisplayFunc(renderScene);
+	glutIdleFunc(renderScene);
+	glutTimerFunc(40, MyTimer, 1);
+	glutReshapeFunc(changeSize);
 
-	glutAttachMenu(GLUT_RIGHT_BUTTON);
-	InitGL();
-	glutDisplayFunc(DrawGLScene);
-	glutReshapeFunc(MyReshape);
-	glutMouseFunc(MyMouse);
-	glutMotionFunc(MyMotion);
-	glutSpecialFunc(MySpecial);
 	glutMainLoop();
-	return 0;
+	if (LoadGLTextures("Data/monalisa.bmp")) {
+		glEnable(GL_TEXTURE_2D);
+		glShadeModel(GL_SMOOTH);
+		glClearDepth(1.0);
+		glEnable(GL_DEPTH_TEST);
+		glDepthFunc(GL_LEQUAL);
+		glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+		glutMainLoop();
+	}
+	return(0);
 }
